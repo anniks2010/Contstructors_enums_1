5ef06c0499e2a53a15194e7d3155d1cc1848a5d2
Opilane TTHK <opilane@tthk.ee>
2019-11-15 09:14:53 +0200

    updates

--- Class_Task/Program.cs
+++ Class_Task/Program.cs
@@ -9,11 +9,13 @@ namespace Class_Task
 {
     class Task
     {
+        public static int Count = 0;
         public string description;
 
         public Task(string _description)
         {
             description = _description;
+            Count++;
         }
     }
     class Program
@@ -21,29 +23,66 @@ namespace Class_Task
         static void Main(string[] args)
         {
             string filePath = @"C:\Demo\task_list.txt";
-            List<string> toDoListInFile = File.ReadAllLines(filePath).ToList();
+            List<string> lines = File.ReadAllLines(filePath).ToList();
 
-            List<Task> listOfTasks = new List<Task>();
+            List<Task> taskList = new List<Task>();
 
-            foreach (string line in toDoListInFile)
+            foreach(string line in lines)
             {
-                Task newToDoList = new Task();
-                
-                Console.WriteLine(line);
+                //Create a task object at the each line that we read from the file.
+                Task newTask = new Task(line); ///line on sõne, mida tahan kasutada objekti koostamisel.
+                //add object to the tasklist
+                taskList.Add(newTask);
+            }
+            int i = 1;
+            int j = 1;
+
+            foreach (Task task in taskList)
+            {
+                Console.WriteLine($"Task {i} on your ToDo list is to {task.description}");
+                i++;
+            }
+            ///kasutajalt saab uue taski küsida
+            Console.WriteLine("Add a new task: ");
+            //salvestada kasutaja antud task
+            string userTaskInput = Console.ReadLine();
+            //teha uus task objekt kasutaja pool tantud sisendiga
+            Task userTask = new Task(userTaskInput);
+            ///salvestada usertask to the taskList
+            taskList.Add(userTask);
+
+            Console.WriteLine("\n Updated task list: ");
+
+            foreach (Task task in taskList)
+            {
+                Console.WriteLine($"Task {j} on your ToDo list is to {task.description}");
+                j++;
+            }
+            ///create a new list of string to wrietto the file
+            List<string> outputToWriteToFile = new List<string>();
+
+            foreach(Task task in taskList)
+            {
+                outputToWriteToFile.Add($"{task.description}");
             }
 
-
-
-
-
-          
-
-            int i = 0;
-
-           
-
-
+            Console.WriteLine("Writing to a file....");
+            ///Write  outputToWriteToFile
+            File.WriteAllLines(filePath, outputToWriteToFile);
+            Console.WriteLine("Your task has been added.");
             Console.ReadLine();
+
+            ////Uue aasta lubadused. Tühi fail teha TXT. paken kasutajale, et ta saaks lisada oma lubatusi ning kasutada classi, objektidena salvestada. Üks omadus klassis, description. Saab lisada ning eemaldada soove listist. Proovi kasutada funktsioone või meetodeid asjadele, mis korduvad
+
+
+
+
+
+
+
+
+
+            
         }
     }
 }
